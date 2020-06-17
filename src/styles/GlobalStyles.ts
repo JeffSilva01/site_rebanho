88a4ab8d40e3e181bffc3a547e227f7f11a6b42e
Jefferson Silva <jeff_silva01@yahoo.com>
2020-06-17 20:38:58 -0300

    add global styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    outline: 0;
+
+    font-family: 'Roboto', sans-serif;
+  }
+
+  :root {
+    --primary: #f6a31c;
+    --secondary: #581518;
+
+    --black: #000;
+    --white: #fff;
+  }
+
+`;
